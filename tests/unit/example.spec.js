c8adf4002fd9d719e8e810b7655ccaf220d61c53
Ethan Wolz <ethan@creadiv.io>
2021-01-13 20:23:14 -0700

    feat: initial commit

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,3 +1,4 @@
+import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
@@ -7,6 +8,6 @@ describe("HelloWorld.vue", () => {
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).to.include(msg);
   });
 });
