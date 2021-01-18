61cb8e6dedf0e3dccd7ecccced3172c244b0833b
Ethan Wolz <ethan@creadiv.io>
2021-01-17 21:22:32 -0700

    docs(home): update home page documentation page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,10 @@ module.exports = {
   description: "A flexible calendar component for VueJS applications",
   themeConfig: {
     sidebar: ["/introduction", "/getting-started", "/props"]
+  },
+  markdown: {
+    toc: {
+      includeLevel: [1, 3]
+    }
   }
 };
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,4 @@
-hello there
+# Welcome
 
-[[toc]]
+:tada: Welcome to WCalendar. The super simple yet elegant solution for your date picking needs!
+Get started by searching in the top right or follow our [Getting Started](/getting-started) instructions.
